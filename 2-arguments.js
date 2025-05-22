5f7f08d047e64079e85ce37c587efb358866a716
Ezebong Matthew <ezebongmatthew490@gmail.com>
2025-05-22 10:14:09 +0100

    2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,3 +1,8 @@
 const arg = process.argv.slice(2);
-const result = arg.length === 0 ? "No argument" : "Arguments found"
-console.log(result);
+if (arg.length === 0) {
+  console.log("No argument");
+} else if (arg.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
