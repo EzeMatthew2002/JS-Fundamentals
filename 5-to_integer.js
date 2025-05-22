80931de05ccb7dc3a2b137f5ce2a3d57d735a3e3
Ezebong Matthew <ezebongmatthew490@gmail.com>
2025-05-22 11:46:14 +0100

    5-to_integer.js

---
+++ 5-to_integer.js
@@ -0,0 +1,10 @@
+const arg = process.argv.slice(2);
+const num = Number(arg[0]);
+
+if (isNaN(num)) {
+  console.log("Not a number");
+} else {
+  console.log(`My number: ${Math.floor(num)}`);
+}
+
+
