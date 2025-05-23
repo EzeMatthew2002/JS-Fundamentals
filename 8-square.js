db28806dd87172ea2f41f4f5bc0c34d663247639
Ezebong Matthew <ezebongmatthew490@gmail.com>
2025-05-23 10:54:05 +0100

    8-square.js

--- 8-square.js
+++ 8-square.js
@@ -5,7 +5,7 @@ function square(n) {
   if (argArr === undefined || isNaN(argArr)) {
     console.log("Missing size");
   } else {
-    for (let i = 0; i <= n; i++) {
+    for (let i = 0; i < n; i++) {
       console.log("X".repeat(n));
     }
   }
